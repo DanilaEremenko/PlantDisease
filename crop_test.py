37bdd92ccade8cfea64ad9ba809ba51f774c2873
danila <danilaeremenko@mail.ru>
2019-07-30 14:14:26 +0300

    -cropping updated, localizing added

--- crop_test.py
+++ crop_test.py
@@ -4,6 +4,7 @@ if __name__ == '__main__':
     img_shape = (32, 32)
     crop_img_window(
         path_img_in="Datasets/PotatoFields/plan/DJI_0246.JPG",
+        img_shape=(512, 512),
         window_shape=img_shape,
         path_out_dir="Datasets/PotatoFields/plan_processed"
     )
--- img_proc.py
+++ img_proc.py
@@ -26,27 +26,51 @@ def crop_multiply_data(img, name, crop_area, path_out_dir):
     pass
 
 
-def crop_img_window(path_img_in, window_shape, path_out_dir):
-    full_img = Image.open(path_img_in)
+def crop_img_window(path_img_in, img_shape, window_shape, path_out_dir, draw_net=True, make_test_dir=False):
+    if not os.path.isdir(path_out_dir):
+        raise Exception("No such directory %s" % path_out_dir)
+
+    full_img = Image.open(path_img_in)  # TODO
+    full_img.thumbnail(img_shape)
+    full_img.show()
     p1_x, p1_y, p2_x, p2_y = 0, 0, window_shape[0], window_shape[1]
     i = 0
     while p2_y <= full_img.size[1]:
         while p2_x <= full_img.size[0]:
-            # crop_multiply_data(img=full_img,
-            #                    name="%d" % name_i,
-            #                    crop_area=(p1_x, p1_y, p2_x, p2_y),
-            #                    path_out_dir=path_out_dir
-            #                    )
+            if make_test_dir:
+                crop_multiply_data(img=full_img,
+                                   name="%d" % i,
+                                   crop_area=(p1_x, p1_y, p2_x, p2_y),
+                                   path_out_dir=path_out_dir
+                                   )
+
+            if draw_net: full_img = draw_rect(full_img, (p1_x, p1_y, p2_x, p2_y), color=255)
+
             p1_x += window_shape[0]
             p2_x += window_shape[0]
-            print "%d:p1_x = %d, p1_y = %d, p2_x = %d, p2_y = %d" % (i, p1_x, p1_y, p2_x, p2_y)
             i += 1
         p1_x = 0
         p2_x = window_shape[0]
         p1_y += window_shape[1]
         p2_y += window_shape[1]
-
+    if draw_net: full_img.show()
     pass
+
+
+#############################################################################
+# --------------------------- localizing ---------------------------------------
+#############################################################################
+def draw_rect(img, points, color):
+    if points.__len__() != 4:
+        raise Exception("points.__len__()!=4")
+    px1, py1, px2, py2 = points[0:4]
+    img_arr = np.asarray(img)
+    img_arr.setflags(write=1)
+    img_arr[py1:py1 + 1, px1:px2] = color  # top
+    img_arr[py2:py2 + 1, px1:px2] = color  # bottom
+    img_arr[py1:py2, px1:px1 + 1] = color  # left
+    img_arr[py1:py2, px2:px2 + 1] = color  # left
+    return Image.fromarray(img_arr)
 
 
 #############################################################################
