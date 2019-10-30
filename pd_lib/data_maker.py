-pd_lib/data_maker get_x_from_croped_image bound calculation fixed

pd_lib/data_maker.py
@@ -103,15 +103,15 @@ def get_x_from_croped_img(path_img_in, img_shape, window_shape, step=1.0, color=
             x_data = np.append(x_data, curr_x)
             x_coord = np.append(x_coord, (p1_x, p1_y, p2_x, p2_y))
 
-            draw_image = draw_rect_on_image(draw_image, (p1_x - 1, p1_y - 1, p2_x - 1, p2_y - 1), color=color)
+            draw_image = draw_rect_on_image(draw_image, (p1_x, p1_y, p2_x, p2_y), color=color)
 
-            p1_x += int(window_shape[0] * step)
-            p2_x += int(window_shape[0] * step)
+            p1_x += int(window_shape[0] * step) + 1
+            p2_x += int(window_shape[0] * step) + 1
             i += 1
         p1_x = 0
         p2_x = window_shape[0]
-        p1_y += int(window_shape[1] * step)
-        p2_y += int(window_shape[1] * step)
+        p1_y += int(window_shape[1] * step) + 1
+        p2_y += int(window_shape[1] * step) + 1
 
     x_data.shape = (i, window_shape[0], window_shape[1], window_shape[2])
     x_coord.shape = (i, 4)