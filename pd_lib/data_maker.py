4d089a65eb2e7bac64eacf91cdb4f6ebfc5dec84
DanilaEremenko <danilaeremenko@mail.ru>
2019-11-12 12:47:13 +0300

    -pd_lib/bug with rectangle cropping fixed

--- pd_lib/data_maker.py
+++ pd_lib/data_maker.py
@@ -105,13 +105,13 @@ def get_x_from_croped_img(path_img_in, img_shape, window_shape, step=1.0, color=
 
             draw_image = draw_rect_on_image(draw_image, (p1_x, p1_y, p2_x, p2_y), color=color)
 
-            p1_x += int(window_shape[0] * step) + 1
-            p2_x += int(window_shape[0] * step) + 1
+            p1_x += int(window_shape[0] * step)
+            p2_x += int(window_shape[0] * step)
             i += 1
         p1_x = 0
         p2_x = window_shape[0]
-        p1_y += int(window_shape[1] * step) + 1
-        p2_y += int(window_shape[1] * step) + 1
+        p1_y += int(window_shape[1] * step)
+        p2_y += int(window_shape[1] * step)
 
     x_data.shape = (i, window_shape[0], window_shape[1], window_shape[2])
     x_coord.shape = (i, 4)
