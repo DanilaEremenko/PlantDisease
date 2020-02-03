5bd48adaba09fdeb30bd439eb579e933f3714147
Danila <danilaeremenko@mail.ru>
2020-02-04 00:34:11 +0300

    universality_via_config_files: json parsing added

--- main_create_json_from_image.py
+++ main_create_json_from_image.py
@@ -1,12 +1,20 @@
 import sys
 from PyQt5 import QtWidgets
 from pd_gui.gui_train_form import WindowClassificationPicture
+import json
+import os
 
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
 
-    window_class_pctr = WindowClassificationPicture()
+    if len(sys.argv) != 2:
+        raise Exception("Unexpected number of arguments")
+
+    with open(os.path.abspath(sys.argv[1])) as config_fp:
+        config_dict = json.load(config_fp)
+
+    window_class_pctr = WindowClassificationPicture(config_dict)
 
     sys.exit(app.exec_())
 
